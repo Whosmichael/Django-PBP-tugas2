9c858abece13de879f98dea9903a6cce4346cde7
Michael Baptiswa Marully <baptiswamichael@gmail.com>
2022-09-14 10:45:28 +0700

    Telah menyelesaikan tugas 2 pbp

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# TODO: Create your views here.
+from katalog.models import CatalogItem
+# Create your views here.
+def show_catalog(request):
+    return render(request, "Katalog.html", context)
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Michael Baptiswa',
+    'npm' : '2106752054'
+}
